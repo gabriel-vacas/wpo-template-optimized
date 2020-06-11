469783ccb9ca306d2566af2c89b95ee2c7d82dc6
Gabriel Vacas <gabriel.vacas@verndale.com>
2020-06-10 19:43:09 -0500

    js opt

--- assets/js/util.js
+++ assets/js/util.js
@@ -4,35 +4,35 @@
 	 * Generate an indented list of links from a nav. Meant for use with panel().
 	 * @return {jQuery} jQuery object.
 	 */
-	$.fn.navList = function() {
+	// $.fn.navList = function() {
 
-		var	$this = $(this);
-			$a = $this.find('a'),
-			b = [];
+	// 	var	$this = $(this);
+	// 		$a = $this.find('a'),
+	// 		b = [];
 
-		$a.each(function() {
+	// 	$a.each(function() {
 
-			var	$this = $(this),
-				indent = Math.max(0, $this.parents('li').length - 1),
-				href = $this.attr('href'),
-				target = $this.attr('target');
+	// 		var	$this = $(this),
+	// 			indent = Math.max(0, $this.parents('li').length - 1),
+	// 			href = $this.attr('href'),
+	// 			target = $this.attr('target');
 
-			b.push(
-				'<a ' +
-					'class="link depth-' + indent + '"' +
-					( (typeof target !== 'undefined' && target != '') ? ' target="' + target + '"' : '') +
-					( (typeof href !== 'undefined' && href != '') ? ' href="' + href + '"' : '') +
-				'>' +
-					'<span class="indent-' + indent + '"></span>' +
-					$this.text() +
-				'</a>'
-			);
+	// 		b.push(
+	// 			'<a ' +
+	// 				'class="link depth-' + indent + '"' +
+	// 				( (typeof target !== 'undefined' && target != '') ? ' target="' + target + '"' : '') +
+	// 				( (typeof href !== 'undefined' && href != '') ? ' href="' + href + '"' : '') +
+	// 			'>' +
+	// 				'<span class="indent-' + indent + '"></span>' +
+	// 				$this.text() +
+	// 			'</a>'
+	// 		);
 
-		});
+	// 	});
 
-		return b.join('');
+	// 	return b.join('');
 
-	};
+	// };
 
 	/**
 	 * Panel-ify an element.
@@ -122,14 +122,14 @@
 						window.setTimeout(function() {
 
 							// Reset scroll position.
-								if (config.resetScroll)
-									$this.scrollTop(0);
+							// 	if (config.resetScroll)
+							// 		$this.scrollTop(0);
 
-							// Reset forms.
-								if (config.resetForms)
-									$this.find('form').each(function() {
-										this.reset();
-									});
+							// // Reset forms.
+							// 	if (config.resetForms)
+							// 		$this.find('form').each(function() {
+							// 			this.reset();
+							// 		});
 
 						}, config.delay);
 
@@ -146,151 +146,151 @@
 					$this.find('a')
 						.css('-webkit-tap-highlight-color', 'rgba(0,0,0,0)');
 
-					$this
-						.on('click', 'a', function(event) {
+					// $this
+					// 	.on('click', 'a', function(event) {
 
-							var $a = $(this),
-								href = $a.attr('href'),
-								target = $a.attr('target');
+					// 		var $a = $(this),
+					// 			href = $a.attr('href'),
+					// 			target = $a.attr('target');
 
-							if (!href || href == '#' || href == '' || href == '#' + id)
-								return;
+					// 		if (!href || href == '#' || href == '' || href == '#' + id)
+					// 			return;
 
-							// Cancel original event.
-								event.preventDefault();
-								event.stopPropagation();
+					// 		// Cancel original event.
+					// 			event.preventDefault();
+					// 			event.stopPropagation();
 
-							// Hide panel.
-								$this._hide();
+					// 		// Hide panel.
+					// 			$this._hide();
 
-							// Redirect to href.
-								window.setTimeout(function() {
+					// 		// Redirect to href.
+					// 			window.setTimeout(function() {
 
-									if (target == '_blank')
-										window.open(href);
-									else
-										window.location.href = href;
+					// 				if (target == '_blank')
+					// 					window.open(href);
+					// 				else
+					// 					window.location.href = href;
 
-								}, config.delay + 10);
+					// 			}, config.delay + 10);
 
-						});
+					// 	});
 
 				}
 
 			// Event: Touch stuff.
-				$this.on('touchstart', function(event) {
+				// $this.on('touchstart', function(event) {
 
-					$this.touchPosX = event.originalEvent.touches[0].pageX;
-					$this.touchPosY = event.originalEvent.touches[0].pageY;
+				// 	$this.touchPosX = event.originalEvent.touches[0].pageX;
+				// 	$this.touchPosY = event.originalEvent.touches[0].pageY;
 
-				})
+				// })
 
-				$this.on('touchmove', function(event) {
+				// $this.on('touchmove', function(event) {
 
-					if ($this.touchPosX === null
-					||	$this.touchPosY === null)
-						return;
+				// 	if ($this.touchPosX === null
+				// 	||	$this.touchPosY === null)
+				// 		return;
 
-					var	diffX = $this.touchPosX - event.originalEvent.touches[0].pageX,
-						diffY = $this.touchPosY - event.originalEvent.touches[0].pageY,
-						th = $this.outerHeight(),
-						ts = ($this.get(0).scrollHeight - $this.scrollTop());
+				// 	var	diffX = $this.touchPosX - event.originalEvent.touches[0].pageX,
+				// 		diffY = $this.touchPosY - event.originalEvent.touches[0].pageY,
+				// 		th = $this.outerHeight(),
+				// 		ts = ($this.get(0).scrollHeight - $this.scrollTop());
 
-					// Hide on swipe?
-						if (config.hideOnSwipe) {
+				// 	// Hide on swipe?
+				// 		if (config.hideOnSwipe) {
 
-							var result = false,
-								boundary = 20,
-								delta = 50;
+				// 			var result = false,
+				// 				boundary = 20,
+				// 				delta = 50;
 
-							switch (config.side) {
+				// 			switch (config.side) {
 
-								case 'left':
-									result = (diffY < boundary && diffY > (-1 * boundary)) && (diffX > delta);
-									break;
+				// 				case 'left':
+				// 					result = (diffY < boundary && diffY > (-1 * boundary)) && (diffX > delta);
+				// 					break;
 
-								case 'right':
-									result = (diffY < boundary && diffY > (-1 * boundary)) && (diffX < (-1 * delta));
-									break;
+				// 				case 'right':
+				// 					result = (diffY < boundary && diffY > (-1 * boundary)) && (diffX < (-1 * delta));
+				// 					break;
 
-								case 'top':
-									result = (diffX < boundary && diffX > (-1 * boundary)) && (diffY > delta);
-									break;
+				// 				case 'top':
+				// 					result = (diffX < boundary && diffX > (-1 * boundary)) && (diffY > delta);
+				// 					break;
 
-								case 'bottom':
-									result = (diffX < boundary && diffX > (-1 * boundary)) && (diffY < (-1 * delta));
-									break;
+				// 				case 'bottom':
+				// 					result = (diffX < boundary && diffX > (-1 * boundary)) && (diffY < (-1 * delta));
+				// 					break;
 
-								default:
-									break;
+				// 				default:
+				// 					break;
 
-							}
+				// 			}
 
-							if (result) {
+				// 			if (result) {
 
-								$this.touchPosX = null;
-								$this.touchPosY = null;
-								$this._hide();
+				// 				$this.touchPosX = null;
+				// 				$this.touchPosY = null;
+				// 				$this._hide();
 
-								return false;
+				// 				return false;
 
-							}
+				// 			}
 
-						}
+				// 		}
 
-					// Prevent vertical scrolling past the top or bottom.
-						if (($this.scrollTop() < 0 && diffY < 0)
-						|| (ts > (th - 2) && ts < (th + 2) && diffY > 0)) {
+				// 	// Prevent vertical scrolling past the top or bottom.
+				// 		if (($this.scrollTop() < 0 && diffY < 0)
+				// 		|| (ts > (th - 2) && ts < (th + 2) && diffY > 0)) {
 
-							event.preventDefault();
-							event.stopPropagation();
+				// 			event.preventDefault();
+				// 			event.stopPropagation();
 
-						}
+				// 		}
 
-				});
+				// });
 
 			// Event: Prevent certain events inside the panel from bubbling.
-				$this.on('click touchend touchstart touchmove', function(event) {
-					event.stopPropagation();
-				});
+				// $this.on('click touchend touchstart touchmove', function(event) {
+				// 	event.stopPropagation();
+				// });
 
 			// Event: Hide panel if a child anchor tag pointing to its ID is clicked.
-				$this.on('click', 'a[href="#' + id + '"]', function(event) {
+				// $this.on('click', 'a[href="#' + id + '"]', function(event) {
 
-					event.preventDefault();
-					event.stopPropagation();
+				// 	event.preventDefault();
+				// 	event.stopPropagation();
 
-					config.target.removeClass(config.visibleClass);
+				// 	config.target.removeClass(config.visibleClass);
 
-				});
+				// });
 
 		// Body.
 
 			// Event: Hide panel on body click/tap.
-				$body.on('click touchend', function(event) {
-					$this._hide(event);
-				});
+				// $body.on('click touchend', function(event) {
+				// 	$this._hide(event);
+				// });
 
 			// Event: Toggle.
-				$body.on('click', 'a[href="#' + id + '"]', function(event) {
+				// $body.on('click', 'a[href="#' + id + '"]', function(event) {
 
-					event.preventDefault();
-					event.stopPropagation();
+				// 	event.preventDefault();
+				// 	event.stopPropagation();
 
-					config.target.toggleClass(config.visibleClass);
+				// 	config.target.toggleClass(config.visibleClass);
 
-				});
+				// });
 
 		// Window.
 
 			// Event: Hide on ESC.
-				if (config.hideOnEscape)
-					$window.on('keydown', function(event) {
+				// if (config.hideOnEscape)
+				// 	$window.on('keydown', function(event) {
 
-						if (event.keyCode == 27)
-							$this._hide(event);
+				// 		if (event.keyCode == 27)
+				// 			$this._hide(event);
 
-					});
+				// 	});
 
 		return $this;
 
@@ -324,195 +324,195 @@
 			var $this = $(this);
 
 		// Text, TextArea.
-			$this.find('input[type=text],textarea')
-				.each(function() {
+			// $this.find('input[type=text],textarea')
+			// 	.each(function() {
 
-					var i = $(this);
+			// 		var i = $(this);
 
-					if (i.val() == ''
-					||  i.val() == i.attr('placeholder'))
-						i
-							.addClass('polyfill-placeholder')
-							.val(i.attr('placeholder'));
+			// 		if (i.val() == ''
+			// 		||  i.val() == i.attr('placeholder'))
+			// 			i
+			// 				.addClass('polyfill-placeholder')
+			// 				.val(i.attr('placeholder'));
 
-				})
-				.on('blur', function() {
+			// 	})
+			// 	.on('blur', function() {
 
-					var i = $(this);
+			// 		var i = $(this);
 
-					if (i.attr('name').match(/-polyfill-field$/))
-						return;
+			// 		if (i.attr('name').match(/-polyfill-field$/))
+			// 			return;
 
-					if (i.val() == '')
-						i
-							.addClass('polyfill-placeholder')
-							.val(i.attr('placeholder'));
+			// 		if (i.val() == '')
+			// 			i
+			// 				.addClass('polyfill-placeholder')
+			// 				.val(i.attr('placeholder'));
 
-				})
-				.on('focus', function() {
+			// 	})
+			// 	.on('focus', function() {
 
-					var i = $(this);
+			// 		var i = $(this);
 
-					if (i.attr('name').match(/-polyfill-field$/))
-						return;
+			// 		if (i.attr('name').match(/-polyfill-field$/))
+			// 			return;
 
-					if (i.val() == i.attr('placeholder'))
-						i
-							.removeClass('polyfill-placeholder')
-							.val('');
+			// 		if (i.val() == i.attr('placeholder'))
+			// 			i
+			// 				.removeClass('polyfill-placeholder')
+			// 				.val('');
 
-				});
+			// 	});
 
 		// Password.
-			$this.find('input[type=password]')
-				.each(function() {
+			// $this.find('input[type=password]')
+			// 	.each(function() {
 
-					var i = $(this);
-					var x = $(
-								$('<div>')
-									.append(i.clone())
-									.remove()
-									.html()
-									.replace(/type="password"/i, 'type="text"')
-									.replace(/type=password/i, 'type=text')
-					);
+			// 		var i = $(this);
+			// 		var x = $(
+			// 					$('<div>')
+			// 						.append(i.clone())
+			// 						.remove()
+			// 						.html()
+			// 						.replace(/type="password"/i, 'type="text"')
+			// 						.replace(/type=password/i, 'type=text')
+			// 		);
 
-					if (i.attr('id') != '')
-						x.attr('id', i.attr('id') + '-polyfill-field');
+			// 		if (i.attr('id') != '')
+			// 			x.attr('id', i.attr('id') + '-polyfill-field');
 
-					if (i.attr('name') != '')
-						x.attr('name', i.attr('name') + '-polyfill-field');
+			// 		if (i.attr('name') != '')
+			// 			x.attr('name', i.attr('name') + '-polyfill-field');
 
-					x.addClass('polyfill-placeholder')
-						.val(x.attr('placeholder')).insertAfter(i);
+			// 		x.addClass('polyfill-placeholder')
+			// 			.val(x.attr('placeholder')).insertAfter(i);
 
-					if (i.val() == '')
-						i.hide();
-					else
-						x.hide();
+			// 		if (i.val() == '')
+			// 			i.hide();
+			// 		else
+			// 			x.hide();
 
-					i
-						.on('blur', function(event) {
+			// 		i
+			// 			.on('blur', function(event) {
 
-							event.preventDefault();
+			// 				event.preventDefault();
 
-							var x = i.parent().find('input[name=' + i.attr('name') + '-polyfill-field]');
+			// 				var x = i.parent().find('input[name=' + i.attr('name') + '-polyfill-field]');
 
-							if (i.val() == '') {
+			// 				if (i.val() == '') {
 
-								i.hide();
-								x.show();
+			// 					i.hide();
+			// 					x.show();
 
-							}
+			// 				}
 
-						});
+			// 			});
 
-					x
-						.on('focus', function(event) {
+			// 		x
+			// 			.on('focus', function(event) {
 
-							event.preventDefault();
+			// 				event.preventDefault();
 
-							var i = x.parent().find('input[name=' + x.attr('name').replace('-polyfill-field', '') + ']');
+			// 				var i = x.parent().find('input[name=' + x.attr('name').replace('-polyfill-field', '') + ']');
 
-							x.hide();
+			// 				x.hide();
 
-							i
-								.show()
-								.focus();
+			// 				i
+			// 					.show()
+			// 					.focus();
 
-						})
-						.on('keypress', function(event) {
+			// 			})
+			// 			.on('keypress', function(event) {
 
-							event.preventDefault();
-							x.val('');
+			// 				event.preventDefault();
+			// 				x.val('');
 
-						});
+			// 			});
 
-				});
+			// 	});
 
 		// Events.
-			$this
-				.on('submit', function() {
+			// $this
+			// 	.on('submit', function() {
 
-					$this.find('input[type=text],input[type=password],textarea')
-						.each(function(event) {
+			// 		$this.find('input[type=text],input[type=password],textarea')
+			// 			.each(function(event) {
 
-							var i = $(this);
+			// 				var i = $(this);
 
-							if (i.attr('name').match(/-polyfill-field$/))
-								i.attr('name', '');
+			// 				if (i.attr('name').match(/-polyfill-field$/))
+			// 					i.attr('name', '');
 
-							if (i.val() == i.attr('placeholder')) {
+			// 				if (i.val() == i.attr('placeholder')) {
 
-								i.removeClass('polyfill-placeholder');
-								i.val('');
+			// 					i.removeClass('polyfill-placeholder');
+			// 					i.val('');
 
-							}
+			// 				}
 
-						});
+			// 			});
 
-				})
-				.on('reset', function(event) {
+			// 	})
+			// 	.on('reset', function(event) {
 
-					event.preventDefault();
+			// 		event.preventDefault();
 
-					$this.find('select')
-						.val($('option:first').val());
+			// 		$this.find('select')
+			// 			.val($('option:first').val());
 
-					$this.find('input,textarea')
-						.each(function() {
+			// 		$this.find('input,textarea')
+			// 			.each(function() {
 
-							var i = $(this),
-								x;
+			// 				var i = $(this),
+			// 					x;
 
-							i.removeClass('polyfill-placeholder');
+			// 				i.removeClass('polyfill-placeholder');
 
-							switch (this.type) {
+			// 				switch (this.type) {
 
-								case 'submit':
-								case 'reset':
-									break;
+			// 					case 'submit':
+			// 					case 'reset':
+			// 						break;
 
-								case 'password':
-									i.val(i.attr('defaultValue'));
+			// 					case 'password':
+			// 						i.val(i.attr('defaultValue'));
 
-									x = i.parent().find('input[name=' + i.attr('name') + '-polyfill-field]');
+			// 						x = i.parent().find('input[name=' + i.attr('name') + '-polyfill-field]');
 
-									if (i.val() == '') {
-										i.hide();
-										x.show();
-									}
-									else {
-										i.show();
-										x.hide();
-									}
+			// 						if (i.val() == '') {
+			// 							i.hide();
+			// 							x.show();
+			// 						}
+			// 						else {
+			// 							i.show();
+			// 							x.hide();
+			// 						}
 
-									break;
+			// 						break;
 
-								case 'checkbox':
-								case 'radio':
-									i.attr('checked', i.attr('defaultValue'));
-									break;
+			// 					case 'checkbox':
+			// 					case 'radio':
+			// 						i.attr('checked', i.attr('defaultValue'));
+			// 						break;
 
-								case 'text':
-								case 'textarea':
-									i.val(i.attr('defaultValue'));
+			// 					case 'text':
+			// 					case 'textarea':
+			// 						i.val(i.attr('defaultValue'));
 
-									if (i.val() == '') {
-										i.addClass('polyfill-placeholder');
-										i.val(i.attr('placeholder'));
-									}
+			// 						if (i.val() == '') {
+			// 							i.addClass('polyfill-placeholder');
+			// 							i.val(i.attr('placeholder'));
+			// 						}
 
-									break;
+			// 						break;
 
-								default:
-									i.val(i.attr('defaultValue'));
-									break;
+			// 					default:
+			// 						i.val(i.attr('defaultValue'));
+			// 						break;
 
-							}
-						});
+			// 				}
+			// 			});
 
-				});
+			// 	});
 
 		return $this;
 
@@ -523,65 +523,65 @@
 	 * @param {jQuery} $elements Elements (or selector) to move.
 	 * @param {bool} condition If true, moves elements to the top. Otherwise, moves elements back to their original locations.
 	 */
-	$.prioritize = function($elements, condition) {
+	// $.prioritize = function($elements, condition) {
 
-		var key = '__prioritize';
+	// 	var key = '__prioritize';
 
-		// Expand $elements if it's not already a jQuery object.
-			if (typeof $elements != 'jQuery')
-				$elements = $($elements);
+	// 	// Expand $elements if it's not already a jQuery object.
+	// 		if (typeof $elements != 'jQuery')
+	// 			$elements = $($elements);
 
-		// Step through elements.
-			$elements.each(function() {
+	// 	// Step through elements.
+	// 		$elements.each(function() {
 
-				var	$e = $(this), $p,
-					$parent = $e.parent();
+	// 			var	$e = $(this), $p,
+	// 				$parent = $e.parent();
 
-				// No parent? Bail.
-					if ($parent.length == 0)
-						return;
+	// 			// No parent? Bail.
+	// 				if ($parent.length == 0)
+	// 					return;
 
-				// Not moved? Move it.
-					if (!$e.data(key)) {
+	// 			// Not moved? Move it.
+	// 				if (!$e.data(key)) {
 
-						// Condition is false? Bail.
-							if (!condition)
-								return;
+	// 					// Condition is false? Bail.
+	// 						if (!condition)
+	// 							return;
 
-						// Get placeholder (which will serve as our point of reference for when this element needs to move back).
-							$p = $e.prev();
+	// 					// Get placeholder (which will serve as our point of reference for when this element needs to move back).
+	// 						$p = $e.prev();
 
-							// Couldn't find anything? Means this element's already at the top, so bail.
-								if ($p.length == 0)
-									return;
+	// 						// Couldn't find anything? Means this element's already at the top, so bail.
+	// 							if ($p.length == 0)
+	// 								return;
 
-						// Move element to top of parent.
-							$e.prependTo($parent);
+	// 					// Move element to top of parent.
+	// 						$e.prependTo($parent);
 
-						// Mark element as moved.
-							$e.data(key, $p);
+	// 					// Mark element as moved.
+	// 						$e.data(key, $p);
 
-					}
+	// 				}
 
-				// Moved already?
-					else {
+	// 			// Moved already?
+	// 				else {
 
-						// Condition is true? Bail.
-							if (condition)
-								return;
+	// 					// Condition is true? Bail.
+	// 						if (condition)
+	// 							return;
 
-						$p = $e.data(key);
+	// 					$p = $e.data(key);
 
-						// Move element back to its original location (using our placeholder).
-							$e.insertAfter($p);
+	// 					// Move element back to its original location (using our placeholder).
+	// 						$e.insertAfter($p);
 
-						// Unmark element as moved.
-							$e.removeData(key);
+	// 					// Unmark element as moved.
+	// 						$e.removeData(key);
 
-					}
+	// 				}
 
-			});
+	// 		});
 
-	};
+	// };
 
 })(jQuery);
